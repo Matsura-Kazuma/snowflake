ec4b253c6b66cf91badfaf5f08027ce9e76d23d8
matsura-kazuma-9442 <605-student032@la-study.com>
2025-08-08 11:41:03 +0900

    環境ファイルに置き換えた

--- versions.tf
+++ versions.tf
@@ -1,13 +1,11 @@
 terraform {
-  required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
+      source  = "Snowflake-Labs/snowflake"
       version = "~> 0.28"
     }
   }
