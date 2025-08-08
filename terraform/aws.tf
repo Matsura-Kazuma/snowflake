2757b34600c869da49386133f69bdd235b0d1298
matsura-kazuma-9442 <605-student032@la-study.com>
2025-08-08 11:49:42 +0900

    S3にバケットをあげてみた

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,4 +1,3 @@
-# AWSプロバイダー設定、S3などのリソース
 provider "aws" {
   region     = "us-west-2"
   access_key = var.aws_access_key
@@ -6,6 +5,13 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
+  bucket = "example-bucket-unique-name-12345"  # バケット名は一意でなければなりません
   acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "test_file" {
+  bucket = aws_s3_bucket.example.bucket
+  key    = "test_file.txt"  # アップロードするファイル名
+  body   = "This is a test string for the S3 bucket."  # ファイルの内容（テスト用文字列）
+  acl    = "private"  # オプション：ファイルのアクセス制御
 }
